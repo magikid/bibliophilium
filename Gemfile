38e4ecbe71028d59d59d1fb0e69bc0927e65c0bf
Chris W Jones <chris@christopherjones.us>
2015-07-19 09:55:45 -0400

    Adds dependency on grape
    I added the grape gem to the Gemfile and also played with the config
    files so that it would work correctly.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem "simple_form"
 gem "title"
 gem "uglifier"
 gem "unicorn"
+gem "grape"
 
 group :development do
   gem "spring"
--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,7 @@ module Bibliophilium
     config.active_record.raise_in_transactional_callbacks = true
 
     config.active_job.queue_adapter = :delayed_job
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
   end
 end

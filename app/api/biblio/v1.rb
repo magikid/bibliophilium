2218f2b20c27c66a97413aee2706274dba61de31
Chris W Jones <chris@christopherjones.us>
2015-07-19 12:05:59 -0400

    Adds basic CRUD for books
    I updated the API with CRUD methods for `Book`.

--- app/api/biblio/v1.rb
+++ app/api/biblio/v1.rb
@@ -5,13 +5,61 @@ class Biblio::V1 < Grape::API
   prefix :api
   version 'v1', using: :path
 
+  helpers do
+    def current_library
+      @current_library = Library.find(2)
+    end
+  end
+
   resource :books do
     desc "Returns the books in a library"
-    params do
-      requires :libid, type: Integer, desc: "The library id"
+    get do
+      current_library.books 
     end
-    get :mine do
-      Library.find(params[:libid]).books
+    
+    desc "Returns a single book"
+    params do
+      requires :id, type: Integer, desc: "A book ID"
+    end
+    get ':id' do
+      Book.find(params[:id])
+    end
+
+    desc "Create a new book"
+    params do
+      requires :title, type: String, desc: "The books title"
+      requires :author, type: String, desc: "The books author"
+      requires :isbn, type: String, desc: "The book's ISBN"
+    end
+    post do
+      Book.create!({
+        tite: params[:title],
+        author: params[:author],
+        isbn: params[:isbn]
+      })
+    end
+
+    desc "Update a book"
+    params do
+      requires :id, type: Integer, desc: "The book's ID to update"
+      requires :title, type: String, desc: "The books title"
+      requires :author, type: String, desc: "The books author"
+      requires :isbn, type: String, desc: "The book's ISBN"
+    end
+    put do
+      Book.find(params[:id]).update({
+        title: params[:title],
+        author: params[:author],
+        isbn: params[:isbn]
+      })
+    end
+
+    desc "Delete a book"
+    params do
+      requires :id, type: Integer, desc: "The book's ID to delete"
+    end
+    delete ':id' do
+      Book.find(params[:id]).destroy
     end
   end
 

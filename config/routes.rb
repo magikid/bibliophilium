62d6e42712d2651f4d1369ff3eeee0a724f31145
Chris W Jones <chris@christopherjones.us>
2015-08-10 20:47:48 -0400

    Adds signin/out functions
    This adds the routes and methods needed to authenticate with Github.  If
    the user isn't already registered with my site, they will be created.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Biblio::API => "/"
 
   match "/auth/:provider/callback" => "sessions#create", via: [:get, :post]
+  get "/signout" => "sessions#destroy", as: :signout
 
   root "welcome#index"
 end
